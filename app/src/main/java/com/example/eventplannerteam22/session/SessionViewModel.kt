9b1bf5a63feacf166402dc059347643da28449d8
wbod_k <minakoffroman2015@gmail.com>
2025-07-07 22:01:03 +0300

    Updated: SessionViewModel to have dedicated login function

--- app/src/main/java/com/example/eventplannerteam22/session/SessionViewModel.kt
+++ app/src/main/java/com/example/eventplannerteam22/session/SessionViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.eventplannerteam22.session
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.auth0.jwt.JWT
+import com.example.eventplannerteam22.auth.TokenResponse
+import com.example.eventplannerteam22.profile.Authority
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,6 +16,7 @@ class SessionViewModel @Inject constructor(
     private val sessionRepository: SessionRepository
 ) : ViewModel() {
 
+    private val logTag = "SessionViewModel"
     private val _session = MutableStateFlow<Session>(Session())
     var session = _session.asStateFlow()
 
@@ -31,14 +36,34 @@ class SessionViewModel @Inject constructor(
         }
     }
 
+    fun login(tokenResponse: TokenResponse) {
+        val role = UserRole.parse(
+            JWT.decode(tokenResponse.accessToken)
+                .getClaim("role")
+                .asArray<Authority>(
+                    Authority::class.java
+                )[0]
+        )
+        _session.update {
+            it.copy(
+                accessToken = tokenResponse.accessToken,
+                refreshToken = tokenResponse.refreshToken,
+                expiresIn = tokenResponse.expiresIn,
+                loggedIn = true,
+                userRole = role
+            )
+        }
+        Log.i(logTag, session.value.toString())
+    }
+
     fun clearSession() {
-        sessionRepository.clearSession()
         _session.update {
             it.copy(
                 accessToken = "",
                 refreshToken = "",
                 expiresIn = 0L,
-                loggedIn = false
+                loggedIn = false,
+                userRole = null
             )
         }
     }
