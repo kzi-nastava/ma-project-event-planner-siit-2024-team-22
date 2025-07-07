a030f822bf610dc4dc67feec75c724c4a320c3eb
wbod_k <minakoffroman2015@gmail.com>
2025-07-07 22:05:30 +0300

    Added: implemented basic role based ui

--- app/src/main/java/com/example/eventplannerteam22/events/EventsScreen.kt
+++ app/src/main/java/com/example/eventplannerteam22/events/EventsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.eventplannerteam22.events
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,23 +15,33 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.eventplannerteam22.session.SessionViewModel
+import com.example.eventplannerteam22.session.UserRole
 
 @Composable
 fun EventsScreen(
     navController: NavController,
     paddingValues: PaddingValues,
-    viewModel: EventsViewModel = hiltViewModel()
+    viewModel: EventsViewModel = hiltViewModel(),
+    sessionViewModel: SessionViewModel = hiltViewModel(LocalContext.current as ComponentActivity)
 ) {
     val events = viewModel.events
     val isLoading = viewModel.isLoading
     val hasMoreEvents = viewModel.hasMoreEvents
+    val session = sessionViewModel.session.collectAsState()
 
-    Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -64,14 +75,15 @@ fun EventsScreen(
                 }
             }
         }
-        FloatingActionButton(
-            onClick = { navController.navigate("add_event") },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ) {
-            Icon(Icons.Default.Add, contentDescription = "Add Product")
-        }
+        if (session.value.loggedIn && session.value.userRole == UserRole.Supplier)
+            FloatingActionButton(
+                onClick = { navController.navigate("add_event") },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add Product")
+            }
     }
 }
 
@@ -93,7 +105,10 @@ fun EventCard(
         ) {
             Text(text = event.name, style = MaterialTheme.typography.headlineSmall)
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Max Capacity: ${event.maxCapacity}", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = "Max Capacity: ${event.maxCapacity}",
+                style = MaterialTheme.typography.bodyMedium
+            )
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = "Date: ${event.dateOfEvent}", style = MaterialTheme.typography.bodyMedium)
             Spacer(modifier = Modifier.height(8.dp))
--- app/src/main/java/com/example/eventplannerteam22/solutions/SolutionsScreen.kt
+++ app/src/main/java/com/example/eventplannerteam22/solutions/SolutionsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.eventplannerteam22.solutions
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,23 +9,33 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.eventplannerteam22.session.SessionViewModel
+import com.example.eventplannerteam22.session.UserRole
 
 @Composable
 fun SolutionsScreen(
     navController: NavController,
     paddingValues: PaddingValues,
-    viewModel: SolutionsViewModel = hiltViewModel()
+    viewModel: SolutionsViewModel = hiltViewModel(),
+    sessionViewModel: SessionViewModel = hiltViewModel(LocalContext.current as ComponentActivity)
 ) {
     val solutions = viewModel.solutions
     val isLoading = viewModel.isLoading
     val hasMoreSolutions = viewModel.hasMoreSolutions
+    val session = sessionViewModel.session.collectAsState()
 
-    Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) {
         Column(modifier = Modifier.fillMaxSize()) {
             LazyColumn(
                 modifier = Modifier.weight(1f),
@@ -55,15 +66,15 @@ fun SolutionsScreen(
                 }
             }
         }
-
-        FloatingActionButton(
-            onClick = { navController.navigate("add_solution") },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ) {
-            Icon(Icons.Default.Add, contentDescription = "Add Solution")
-        }
+        if (session.value.loggedIn && session.value.userRole == UserRole.Supplier)
+            FloatingActionButton(
+                onClick = { navController.navigate("add_solution") },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add Solution")
+            }
     }
 }
 
@@ -85,11 +96,17 @@ fun SolutionCard(
         ) {
             Text(text = solution.name, style = MaterialTheme.typography.headlineSmall)
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Category type: ${solution.category.name}", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = "Category type: ${solution.category.name}",
+                style = MaterialTheme.typography.bodyMedium
+            )
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = "Price: ${solution.price}", style = MaterialTheme.typography.bodyMedium)
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Discount: ${solution.discount}", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = "Discount: ${solution.discount}",
+                style = MaterialTheme.typography.bodyMedium
+            )
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = solution.description, style = MaterialTheme.typography.bodyMedium)
         }
