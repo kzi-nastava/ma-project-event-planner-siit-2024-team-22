d67dce124611426904bb8cdcc795fa962828c385
wbod_k <minakoffroman2015@gmail.com>
2025-07-07 22:02:16 +0300

    Added: UserRole sealed class

---
+++ app/src/main/java/com/example/eventplannerteam22/session/UserRole.kt
@@ -0,0 +1,24 @@
+package com.example.eventplannerteam22.session
+
+import com.example.eventplannerteam22.profile.Authority
+
+sealed class UserRole(val label: String) {
+    object User : UserRole("User")
+    object EventOrganizer : UserRole("EventOrganizer")
+    object Supplier : UserRole("Supplier")
+    object Admin : UserRole("Admin")
+
+    override fun toString(): String = label
+
+    companion object {
+        fun parse(authority: Authority): UserRole {
+            return when (authority.authority) {
+                "ROLE_User" -> User
+                "ROLE_Admin" -> Admin
+                "ROLE_EventOrganizer" -> EventOrganizer
+                "ROLE_Supplier" -> Supplier
+                else -> throw IllegalArgumentException("Unknown role: ${authority.authority}")
+            }
+        }
+    }
+}
