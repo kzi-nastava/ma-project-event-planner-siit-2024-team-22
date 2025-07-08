9c129e4e4032d1c1a7ef22c6488369f6c0bcb60d
wbod_k <minakoffroman2015@gmail.com>
2025-07-08 10:35:59 +0300

    Updated: removed unused screens and components

--- app/src/main/java/com/example/eventplannerteam22/presentation/components/CardItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.eventplannerteam22.presentation.components
-
-
-data class CardItem(
-    val imageRes: Int, // Ресурс изображения
-    val title: String,
-    val description: String,
-    val type: String // product, service, event
-)
