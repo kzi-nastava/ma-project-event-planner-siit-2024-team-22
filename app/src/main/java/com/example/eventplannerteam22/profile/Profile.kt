d6e98c348882228045ff59c44e216375306656b2
wbod_k <minakoffroman2015@gmail.com>
2025-07-12 17:16:23 +0200

    Updated: Profile to use apiResultHandler function

--- app/src/main/java/com/example/eventplannerteam22/profile/Profile.kt
+++ app/src/main/java/com/example/eventplannerteam22/profile/Profile.kt
@@ -3,20 +3,20 @@ package com.example.eventplannerteam22.profile
 import com.example.eventplannerteam22.products.Product
 
 data class Profile(
-    val id: Int,
-    val name: String,
-    val surname: String,
-    val email: String,
-    val password: String,
-    val phone: String?,
-    val homeAddress: String?,
-    val role: String,
-    val favoriteProducts: List<Product>,
-    val enabled: Boolean,
-    val username: String,
-    val authorities: List<Authority>,
-    val accountNonExpired: Boolean,
-    val accountNonLocked: Boolean,
-    val credentialsNonExpired: Boolean
+    val id: Int = 0,
+    val name: String = "",
+    val surname: String = "",
+    val email: String = "",
+    val password: String = "",
+    val phone: String? = null,
+    val homeAddress: String? = null,
+    val role: String = "",
+    val favoriteProducts: List<Product>? = null,
+    val enabled: Boolean = false,
+    val username: String = "",
+    val authorities: List<Authority>? = null,
+    val accountNonExpired: Boolean? = null,
+    val accountNonLocked: Boolean? = null,
+    val credentialsNonExpired: Boolean? = null
 )
 
--- app/src/main/java/com/example/eventplannerteam22/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/eventplannerteam22/profile/ProfileViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.eventplannerteam22.profile
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -8,6 +7,8 @@ import androidx.lifecycle.ViewModel
 import com.auth0.jwt.JWT
 import com.example.eventplannerteam22.network.ApiResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,22 +16,49 @@ class ProfileViewModel @Inject constructor(
     private val profileRepository: ProfileRepository,
 ) : ViewModel() {
     private val logTag = "ProfileViewModel"
-    var userProfile by mutableStateOf<Profile?>(null)
+    var userProfile by mutableStateOf(Profile())
         private set
 
+    var screenState by mutableStateOf(ProfileScreenState())
+        private set
+
+    private val resultChannel = Channel<ApiResult<Profile>>()
+    val apiResults = resultChannel.receiveAsFlow()
+
     suspend fun loadUserProfile(accessToken: String) {
+        screenState.isLoading = true
         val userId = JWT.decode(accessToken).getClaim("userId").asInt()
         val result = profileRepository.getProfileById(userId)
-        when (result) {
-            is ApiResult.Success -> {
-                userProfile = result.data
-            }
-
-            else -> Log.e(logTag, "Some kind of error happened")
+        resultChannel.send(result)
+        if (result is ApiResult.Success) {
+            userProfile = result.data
+            screenState.isLoading = false
         }
     }
 
+    fun updateUserProfile(
+        id: Int?,
+        name: String?,
+        surname: String?,
+        email: String?,
+        password: String?,
+        phone: String?,
+        homeAddress: String?,
+        role: String?
+    ) {
+        userProfile = userProfile.copy(
+            id = id ?: userProfile.id,
+            name = name ?: userProfile.name,
+            surname = surname ?: userProfile.surname,
+            email = email ?: userProfile.email,
+            password = password ?: userProfile.password,
+            phone = phone ?: userProfile.phone,
+            homeAddress = homeAddress ?: userProfile.homeAddress,
+            role = role ?: userProfile.role,
+        )
+    }
+
     fun unloadUserProfile() {
-        userProfile = null
+        userProfile = Profile()
     }
 }
