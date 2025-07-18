e246029e92ec61e4cffc79d128f29406ffb7880f
wbod_k <minakoffroman2015@gmail.com>
2025-07-18 15:18:23 +0200

    Fixed: event screen role based ui error

--- app/src/main/java/com/example/eventplannerteam22/events/EventsScreen.kt
+++ app/src/main/java/com/example/eventplannerteam22/events/EventsScreen.kt
@@ -2,7 +2,14 @@ package com.example.eventplannerteam22.events
 
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -75,7 +82,7 @@ fun EventsScreen(
                 }
             }
         }
-        if (session.value.loggedIn && session.value.userRole == UserRole.Supplier)
+        if (session.value.loggedIn && session.value.userRole == UserRole.EventOrganizer)
             FloatingActionButton(
                 onClick = { navController.navigate("add_event") },
                 modifier = Modifier
