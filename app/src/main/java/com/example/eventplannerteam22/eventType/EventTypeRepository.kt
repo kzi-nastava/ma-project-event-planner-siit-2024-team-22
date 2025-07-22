2cde40a12cdef11c8c83586be6c01e413abd030f
wbod_k <minakoffroman2015@gmail.com>
2025-07-22 14:18:01 +0200

    Added: create methods in EventTypeApi.kt and EventTypeRepository.kt

--- app/src/main/java/com/example/eventplannerteam22/eventType/EventTypeRepository.kt
+++ app/src/main/java/com/example/eventplannerteam22/eventType/EventTypeRepository.kt
@@ -4,4 +4,5 @@ import com.example.eventplannerteam22.network.ApiResult
 
 interface EventTypeRepository {
     suspend fun getAllEventTypes(): ApiResult<List<EventType>>
+    suspend fun createEventType(name: String, description: String): ApiResult<Unit>
 }
