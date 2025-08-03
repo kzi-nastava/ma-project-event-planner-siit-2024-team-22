7c0e5bf0aab7f06bca7222aeede07cdf6fd463b4
wbod_k <minakoffroman2015@gmail.com>
2025-08-03 22:50:48 +0200

    Added: event type details draft

---
+++ app/src/main/java/com/example/eventplannerteam22/eventtype/presentation/eventtypedetails/EventTypeDetailsScreen.kt
@@ -0,0 +1,13 @@
+package com.example.eventplannerteam22.eventtype.presentation.eventtypedetails
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun EventTypeDetailsScreen(
+    navController: NavController,
+    viewModel: EventTypeDetailsViewModel = hiltViewModel()
+) {
+
+}
